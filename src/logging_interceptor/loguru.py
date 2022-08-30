71d1f21220ee9815c1d88fd2263d0aeec99dc34e
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2022-08-30 16:40:17 +0200

    Somehow the logger is confused with named arguments

--- src/logging_interceptor/loguru.py
+++ src/logging_interceptor/loguru.py
@@ -23,8 +23,8 @@ class InterceptHandler(logging.Handler):
             depth += 1
 
         logger.opt(depth=depth, exception=record.exc_info).log(
-            level=level,
-            message=record.getMessage(),
+            level,
+            record.getMessage(),
         )
 
 
